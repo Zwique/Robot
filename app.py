c6d70a03d7700084872340f501e06a299af9a148
Erdembileg Ariunbold <97537483+Zwique@users.noreply.github.com>
2025-03-03 20:06:42 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Route for the homepage
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+# Route for robots.txt
+@app.route('/robots.txt')
+def robots():
+    return """
+    User-agent: *
+    Allow: /secret
+    Disallow: /flag.txt
+    """
+
+# Route for flag.txt
+@app.route('/secret/flag.txt')
+def flag():
+    # Open flag.txt in read-only mode and return its content
+    try:
+        with open("flag.txt", "r") as f:
+            return f.read()
+    except FileNotFoundError:
+        return "Flag not found! Contact the administrator.", 404
+
+# Run the app
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=1337)
